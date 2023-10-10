aaf165f8d362ba069e4d10b2f0cd1a240fa991b7
Keiji O <meekunn@gmail.com>
2023-10-10 00:57:58 -0700

    partially set up input form

--- reactPage/src/components/pages/Steel.js
+++ reactPage/src/components/pages/Steel.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
- function Steel() {
-    return(
-        <div>
-            Steel
-        </div>
-    )
+function Steel() {
+  const [inputState, setInputState] = useState({
+    steelType: "",
+    length: "",
+    quantity: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputState({
+      ...inputState,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    //placeholder
+    const { name } = event.target;
+    setInputState({
+      ...inputState,
+      [name]: "",
+    });
+  };
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Type</label>
+          <input
+            type='text'
+            name='steelType'
+            placeholder='input steel type'
+            onChange={handleChange}
+            required
+          />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default Steel;
